758b5bd3a1fec6168294abeff2c5a5b991b5ed25
Christian Glissov <s146996@student.dtu.dk>
2020-03-23 16:15:33 +0100

    databases

---
+++ OnlineShopDB.sql
@@ -0,0 +1,18 @@
+/* The Online Shop Database based on the schema diagram shown in the report. Adapted UML notation is from the textbook 
+"Database System Concepts" by A. Silberschatz, H.F. Korth and S. Sudarshan, McGraw-Hill International Edition, 
+Sixth Edition, 2011. 
+OnlineShopDB.sql is a script for creating tables for the Online Shop database and populating them with data */
+
+/* If the tables already exists, then they are deleted! */
+
+# DROP TABLE IF EXISTS entity
+
+/* Table creation! Create Tables with Foreign Keys after the referenced tables are created! */
+
+
+# CREATE TABLES HERE
+
+
+/* Insertion of table rows one by one! */
+
+# INSERT DATA HERE
