335c3b3ae468560a7016b1cba394623df9d677a0
Melina Barhagh <s164207@student.dtu.dk>
2020-04-03 22:59:45 +0200

    Opgave 9 v.4

--- Opgave9.sql
+++ Opgave9.sql
@@ -2,7 +2,24 @@ USE BSOS;
 SET SQL_SAFE_UPDATES = 0;
 SET GLOBAL event_scheduler = 1;
 
-/* Check if all items are in stock for a specific order_id */
+/* UPDATE */
+SELECT customer_id, customer_first_name, customer_last_name, customer_email, customer_accept_news
+FROM Customer
+WHERE customer_id = 3;
+
+UPDATE Customer
+SET customer_email = 'barhagh@hotmail.com', customer_accept_news = 0
+WHERE customer_id = 3;
+
+/* DELETE */
+SELECT *
+FROM Orders;
+
+DELETE FROM Orders
+WHERE order_date < DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR);
+
+/* FUNCTION 												*/
+/* Check if all items are in stock for a specific order_id  */
 DROP FUNCTION IF EXISTS in_stock;
 DELIMITER //
 CREATE FUNCTION in_stock(orderid INT) RETURNS BOOLEAN
@@ -17,6 +34,10 @@ BEGIN
 END; //
 DELIMITER ;
 
+-- Test case
+SELECT in_stock(7); -- Test if items in order with order_id = 7 is in stock
+
+/* TRIGGER 												  */
 /* Check if all items are in stock before approving order */
 DROP TRIGGER IF EXISTS approve_order;
 DELIMITER //
@@ -28,79 +49,58 @@ BEGIN
 END; // 
 DELIMITER ;
 
-
-/* Transaction that updates the stock in Product according to the quantity in OrderItems */
-DELIMITER //
-DROP PROCEDURE IF EXISTS Stock_update;
-CREATE PROCEDURE Stock_update(
-IN vproduct INT, vquantity INT, approval BOOLEAN, OUT vStatus VARCHAR(45))
-BEGIN
-DECLARE Oldstock, Newstock INT DEFAULT 0; START TRANSACTION;
-SET Oldstock = (SELECT product_stock FROM Product WHERE product_id = vproduct); SET Newstock = Oldstock - vquantity;
-UPDATE Product SET product_stock = Newstock WHERE product_id = vproduct;
-IF (approval)
-THEN SET vStatus = 'Transaction Transfer committed!'; COMMIT;
-ELSE SET vStatus = 'Transaction Transfer rollback'; ROLLBACK; END IF;
-END; // DELIMITER ;
-
-
-
-DROP TRIGGER IF EXISTS update_stock;
-DELIMITER $$
-CREATE TRIGGER update_stock
-AFTER UPDATE ON Orders FOR EACH ROW
-BEGIN
-	DECLARE prodId INT;
-    DECLARE quantity INT;
-    DECLARE approval BOOLEAN;
-	SELECT product_id, order_item_quantity, order_approval INTO ProdId, quantity, approval FROM OrderItem LEFT JOIN Orders using(order_id);
-
-	CALL Stock_update(prodId, quantity, approval, @Status);
-END; $$
-DELIMITER ;
-
-
-/* Event */
-CREATE EVENT Shipped
-ON SCHEDULE EVERY 1 MINUTE -- 15 DAY_HOUR
-DO UPDATE Orders 
-	SET order_shipped = CASE WHEN order_approval = 1 and order_shipped IS NULL THEN CURRENT_TIMESTAMP
-							 ELSE order_shipped END;
-
-
-
+-- Test case
 INSERT Orders VALUES
 (13, 1, '2020-01-01 08:13:00',  FALSE, null, 'mobilepay');
-
-SELECT *
-FROM Product;
-
 INSERT OrderItem VALUES
 (13,1,2),
-(13,12,1);
-
-SELECT *
-FROM Orders;
-
-SELECT *
-FROM Product;
-
-SELECT * FROM OrderItem;
+(13,12,1); -- This item is not in stock, this order_approved is expected to = 0
 
 INSERT Orders VALUES
 (14, 1, '2020-01-01 08:15:00',  FALSE, null, 'mobilepay');
-
 INSERT OrderItem VALUES
 (14,11,1),
-(14,1,2);
+(14,1,2); -- All items are in stock
 
 SELECT *
-FROM Orders;
+FROM Orders; -- Order 13 is denied while order 14 is approved
 
-SELECT * FROM OrderItem;
+/* TRANSACTION										*/
+/* Transaction that updates the stock in Product	*/
+/* according to the quantity in OrderItems 			*/
+DROP PROCEDURE IF EXISTS Stock_update;
+DELIMITER //
+CREATE PROCEDURE Stock_update(IN vproduct INT, vquantity INT, vapproval BOOLEAN, OUT vStatus VARCHAR(45))
+BEGIN
+DECLARE Oldstock, Newstock INT DEFAULT 0; 
+START TRANSACTION;
+	SET Oldstock = (SELECT product_stock FROM Product WHERE product_id = vproduct); SET Newstock = Oldstock - vquantity;
+	UPDATE Product SET product_stock = Newstock WHERE product_id = vproduct;
+	IF (vapproval)
+	THEN SET vStatus = 'Transaction Transfer committed!'; COMMIT;
+	ELSE SET vStatus = 'Transaction Transfer rollback'; ROLLBACK; END IF;
+END; // 
+DELIMITER ;
 
-SELECT *
-FROM Product;
+-- Test case
+SELECT product_id, product_stock
+FROM Product
+WHERE Product_id = 1;
 
-SELECT in_stock(14);
+CALL Stock_update(1,2,1,@Status); -- Changing the stock by -2 of product with id = 1 t (order_approved = 1)
+SELECT @Status;
+
+SELECT product_id, product_stock
+FROM Product
+WHERE Product_id = 1;
+
+CALL Stock_update(1,2,0,@Status); -- order_approved = 0, thus we do not expect a stock update
+SELECT @Status;
+
+/* EVENT  */
+CREATE EVENT Shipped
+ON SCHEDULE EVERY 1 DAY STARTS '2020-04-04 15:00:00' -- Change to 1 MINUTE to see immediate change in order_shipped 
+DO UPDATE Orders 
+	SET order_shipped = CASE WHEN order_approval = 1 and order_shipped IS NULL THEN CURRENT_TIMESTAMP
+							 ELSE order_shipped END;
 
